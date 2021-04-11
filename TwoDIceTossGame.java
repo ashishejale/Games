f68292561cdb4c6ab20f3d9a839f410aa92749a1
Ashish Shejale <70459904+ashishejale@users.noreply.github.com>
2021-04-11 14:05:19 +0530

    Update TwoDIceTossGame.java

--- TwoDIceTossGame.java
+++ TwoDIceTossGame.java
@@ -1,16 +1,16 @@
 import java.util.Random;
 
 public class TwoDiceTossGame {
-    public static void diceToss() { //to generate numbers 0-6
+    public static void twoDiceToss() { //to generate numbers 0-6
         Random randomNumber = new Random();
         int result1 = Math.abs(randomNumber.nextInt()) % 7; //generate a random positive number and find its remainder when divided by 7
         int result2 = Math.abs(randomNumber.nextInt()) % 7; //generate a random positive number and find its remainder when divided by 7
-        System.out.println("( " + result1 + "," + result2 + " )");
+        System.out.println("( " + result1 + " , " + result2 + " )");
     }
     public static void main(String[] args) {
 	// write your code here
         for(int i = 0; i < 5; i++) {
-            diceToss();
+            twoDiceToss();
         }
     }
 }
